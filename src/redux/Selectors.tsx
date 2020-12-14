c113d70ba74e56a12f4f1bb20b3d6f19f341d0c3
Joonas Suurväli <joonas.suurvali@solita.fi>
2020-12-14 12:56:11 +0200

    changed the reducr codes in Selectors.js to be more readable

--- src/redux/Selectors.tsx
+++ src/redux/Selectors.tsx
@@ -4,9 +4,8 @@ export const getProducts = (state: { app: { products: StoreItem[] } }) =>
   state.app.products;
 export const getCart = (state: { app: { cart: Cart[] } }) => {
   const cart = state.app.cart;
-  const cartQuantity = Object.values(cart).reduce(
-    (accumulator, { quantity }) => accumulator + quantity,
-    0
-  );
+  const reducer = (accumulator: number, {quantity}: {quantity: number}) => accumulator + quantity;
+  const cartQuantity = Object.values(cart).reduce(reducer, 0);
+  
   return { cart, cartQuantity };
 };
