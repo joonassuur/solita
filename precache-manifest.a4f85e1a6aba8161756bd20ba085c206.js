e607d00d0ead7e00fa60439ac88175cdacf3dc10
joonas <joonas.suurvali@gmail.com>
2021-06-19 08:47:23 +0300

    Updates

---
+++ precache-manifest.a4f85e1a6aba8161756bd20ba085c206.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30af99e8065ac4f23ed614a89cd6dc7d",
+    "url": "/whee-web-store/index.html"
+  },
+  {
+    "revision": "fc4cd9e781550f1239a3",
+    "url": "/whee-web-store/static/css/3.2bc212ea.chunk.css"
+  },
+  {
+    "revision": "d1f630b2937fee9e147c",
+    "url": "/whee-web-store/static/css/4.2bc212ea.chunk.css"
+  },
+  {
+    "revision": "a763dc8d589995fe5bb2",
+    "url": "/whee-web-store/static/css/5.7bc3a9f2.chunk.css"
+  },
+  {
+    "revision": "7a1da4ffdddcebbb8f69",
+    "url": "/whee-web-store/static/css/6.eeb7568b.chunk.css"
+  },
+  {
+    "revision": "50dadd3f4a892098cd60",
+    "url": "/whee-web-store/static/css/7.8819dd87.chunk.css"
+  },
+  {
+    "revision": "ffb501e287a86581ee20",
+    "url": "/whee-web-store/static/css/main.fb7b4e3b.chunk.css"
+  },
+  {
+    "revision": "9fa2b6f4824e1ed69a1a",
+    "url": "/whee-web-store/static/js/2.d4a1366d.chunk.js"
+  },
+  {
+    "revision": "33490309eac47256b827830c802fd4a5",
+    "url": "/whee-web-store/static/js/2.d4a1366d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc4cd9e781550f1239a3",
+    "url": "/whee-web-store/static/js/3.7ff30268.chunk.js"
+  },
+  {
+    "revision": "c360eff85ca112d098c080b19dff9b78",
+    "url": "/whee-web-store/static/js/3.7ff30268.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1f630b2937fee9e147c",
+    "url": "/whee-web-store/static/js/4.bc169d1f.chunk.js"
+  },
+  {
+    "revision": "a763dc8d589995fe5bb2",
+    "url": "/whee-web-store/static/js/5.b9123de9.chunk.js"
+  },
+  {
+    "revision": "7a1da4ffdddcebbb8f69",
+    "url": "/whee-web-store/static/js/6.0abca078.chunk.js"
+  },
+  {
+    "revision": "50dadd3f4a892098cd60",
+    "url": "/whee-web-store/static/js/7.3f01db73.chunk.js"
+  },
+  {
+    "revision": "ffb501e287a86581ee20",
+    "url": "/whee-web-store/static/js/main.ff7ee249.chunk.js"
+  },
+  {
+    "revision": "b2fc123c9e7b2d536b70",
+    "url": "/whee-web-store/static/js/runtime-main.b3abc42b.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/whee-web-store/precache-manifest.a4f85e1a6aba8161756bd20ba085c206.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/whee-web-store/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
